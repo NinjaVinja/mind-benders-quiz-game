ddc3a445048e45e07cb6df29ca17986febe81638
NinjaVinja <taha.ahmad4700@gmail.com>
2026-07-13 00:26:54 +0500

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self, text, answer):
+        """Initialize attributes to describe the question."""
+        self.text = text
+        self.answer = answer
+new_question = Question("Value of Pi is 3.14 ", "True")
